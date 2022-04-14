c9ddddbbbc3739548889194a5cf4ddaa26cc1801
naimur.a.rahman <naimur.a.rahman@doyenhub.com>
2022-04-14 09:42:09 +0600

    c program

---
+++ loops.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+        int x=0;
+
+
+
+        if(x<0){
+            printf("the number is gatter than Zero\n");
+
+        }else if(x==0){
+            printf("the number is equall to zero");
+        }else{
+            printf("the number is posetive");
+        }
+
+   
+}
