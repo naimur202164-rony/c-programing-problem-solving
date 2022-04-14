b2c35fa8410b4f2ab4e1319d69ca9ed3a0cea1fc
naimur.a.rahman <naimur.a.rahman@doyenhub.com>
2022-04-14 10:53:13 +0600

    vowel added

--- loops.c
+++ loops.c
@@ -27,17 +27,57 @@
 
 
 
+// int main(){
+//     int number,remainder;
+//     number=4;
+//     remainder=number%2;
+
+//     if(remainder==0){
+//         printf("the number is even\n");
+//     }else{
+//         printf("the number is oddd");
+//     }
+
+
+
+// }
+
+
+
+// Finding the Uppercase or LowerCase;
+
+
+// #include <stdio.h>
+
+// int main(){
+
+
+//     int num=0;
+//     if(num>=1||num<=10){
+
+//         printf("yes\n");
+//     }else(
+//         printf("no\n")
+//     );
+// }
+
+// Vowel code
+
+
+
+#include <stdio.h>
+
 int main(){
-    int number,remainder;
-    number=4;
-    remainder=number%2;
 
-    if(remainder==0){
-        printf("the number is even\n");
+    char ch;
+      
+    ch='a';
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+
+        printf("%c is vowel");
+
+
     }else{
-        printf("the number is oddd");
+        printf("%c is not vowel");
     }
-
-
-
 }
