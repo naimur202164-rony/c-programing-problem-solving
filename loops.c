05b1f9558492cd64d644db7c9c1f10f614466964
naimur.a.rahman <naimur.a.rahman@doyenhub.com>
2022-04-14 10:06:02 +0600

    the code is even

--- loops.c
+++ loops.c
@@ -1,19 +1,43 @@
+// #include<stdio.h>
+
+// int main(){
+
+//         int x=0;
+
+
+
+//         if(x<0){
+//             printf("the number is gatter than Zero\n");
+
+//         }else if(x==0){
+//             printf("the number is equall to zero");
+//         }else{
+//             printf("the number is posetive");
+//         }
+
+// }
+
+
+
+
+
+
 #include<stdio.h>
 
+
+
+
 int main(){
+    int number,remainder;
+    number=4;
+    remainder=number%2;
 
-        int x=0;
+    if(remainder==0){
+        printf("the number is even\n");
+    }else{
+        printf("the number is oddd");
+    }
 
 
 
-        if(x<0){
-            printf("the number is gatter than Zero\n");
-
-        }else if(x==0){
-            printf("the number is equall to zero");
-        }else{
-            printf("the number is posetive");
-        }
-
-   
 }
