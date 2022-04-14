fd478e515b397a50bc217bd186ec8e7ebf5431ba
naimur.a.rahman <naimur.a.rahman@doyenhub.com>
2022-04-14 11:18:03 +0600

    for lop added

--- loops.c
+++ loops.c
@@ -87,28 +87,45 @@
 
 
 
+// #include<stdio.h>
+
+// int main(){
+
+
+// int x=1;
+
+// while (x<=20)
+// {
+//         printf("%d\n",x);
+//             /* code */
+//         x=x+1;
+
+
+//         if(x>10){
+//             break;
+//         };
+
+
+//         return 0;
+
+
+// }
+
+// }
+
+
 #include<stdio.h>
 
 int main(){
 
-
-int x=1;
-
-while (x<=20)
-{
-        printf("%d\n",x);
-            /* code */
-        x=x+1;
+    int n=5;
+    int i=1;
 
 
-        if(x>10){
-            break;
-        };
-
-
-        return 0;
-
-
-}
-
+    while (n<=10)
+    {
+      printf("%dX%d=%d\n",n,i,n*i);
+      i++;
+    }
+    
 }
